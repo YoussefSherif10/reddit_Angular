edc3913fe478a07bf84516dc5f10deb5a9e0481c
yousseffekry <mr-youssefsherif@hotmail.com>
2023-04-09 18:10:29 +0200

    Added bootstrap and created the form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserItemComponent } from './components/user-item/user-item.component';
-import { UserListComponent } from './components/user-list/user-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserItemComponent,
-    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
