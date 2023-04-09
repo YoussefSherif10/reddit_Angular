8b50ae978db6117af74711e2fb08742f3acc9522
yousseffekry <mr-youssefsherif@hotmail.com>
2023-04-09 16:39:28 +0200

    rendered user item from user list component

--- src/app/components/user-item/user-item.component.ts
+++ src/app/components/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-item.component.css']
 })
 export class UserItemComponent {
-  name : string = 'Youssef';
+  @Input() name !: string;
 
 }
