afd4cfa4532b37e74e738290f5d8eee3418d3032
yousseffekry <mr-youssefsherif@hotmail.com>
2023-04-09 16:08:04 +0200

    Generated the user item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserItemComponent } from './components/user-item/user-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule
